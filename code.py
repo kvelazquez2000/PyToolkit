22c77625265bbcb0a61c27e98f6ebc379216176b
Kyle <kvelazquez6216@students.mchenry.edu>
2026-09-13 20:58:03 -0500

    update reflections collects and word stats

--- code.py
+++ code.py
@@ -26,7 +26,7 @@ def word_stats(words):
     counter = Counter(w.lower().strip(".,!?") for w in words if w)
     by_prefix = defaultdict(list)
     for w in counter:
-        by_prefix[w[:1]].append(w)
+        by_prefix[w[0]].append(w)
     return counter, by_prefix
 
 def main():
